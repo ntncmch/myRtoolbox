99455af249d1d3febc263ca6a489e93e3e55eb4e
Anton <anton.camacho@lshtm.ac.uk>
2014-06-23 20:44:22 +0100

    new functions to plot

--- R/plot.r
+++ R/plot.r
@@ -7,3 +7,16 @@
 vplayout <- function(x, y){
 	viewport(layout.pos.row = x, layout.pos.col = y)
 }
+
+
+#'Add letter
+#'
+#'Add letter to a ggplot
+#' @param letter character
+#' @param margin vector of size 4
+#' @inheritParams ggplot2::element_text
+#' @export
+#' @import ggplot2
+ggletter <- function(letter, margin=c(0,0.2,0,0), size= 20, hjust= -0.075, vjust = -0.1) {
+	ggtitle(letter) + theme(plot.title = element_text(size = size, hjust = hjust, vjust = vjust, face = "bold"), plot.margin = unit(margin,"cm"))
+}
