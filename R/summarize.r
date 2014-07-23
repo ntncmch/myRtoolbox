0f88c437e90c0f5b75b767008315d7df3bde8542
Anton <anton.camacho@lshtm.ac.uk>
2014-07-23 14:42:43 +0100

    edit summarize with NA

--- R/summarize.r
+++ R/summarize.r
@@ -1,27 +1,54 @@
 #'Basic accounting for NA values
 #'
-#'All these functions performs the same as the basic version but first remove all \code{NA} elements. If all elements are \code{NA} then \code{NA} is returned, which is not always the case for the basic function.
+#'All these functions performs the same as the basic version but differ when all elements are \code{NA}. In this case, \code{NA} is returned in the same format as \code{x}, which is not always the case when the basic function is used with \code{\link[dplyr]{summarize}}.
 #' @param x vector
+#' @param na_rm logical. If \code{TRUE}, \code{\link{na.omit}} is performed first. Default to \code{FALSE}
 #' @export
 #' @importFrom dplyr last
-#' @name NA omit and summarize 
+#' @name summarize with NA
 #' @export
-#' @aliases last_na_omit
-last_na_omit <- function(x) {
+#' @aliases last_na
+last_na <- function(x,na_rm=FALSE) {
 
 	if(all(is.na(x))){
-		x <- x[1]
-	} else {
-		x <- last(na.omit(x))
-	}
+		return(x[1])
+	} 
 
-	return(x)
+	if(na_rm){
+		x <- na.omit(x)
+	}
+	
+	return(last(x))	
 }
 
 
-#' @name NA omit and summarize
+#' @name summarize with NA
 #' @export
-#' @aliases any_na_omit
-any_na_omit <- function(x) {
-	any(x,na.rm=!all(is.na(x)))
+#' @aliases any_na
+any_na <- function(x,na_rm=FALSE) {
+
+	if(all(is.na(x))){
+		return(x[1])
+	} 
+
+	return(any(x,na.rm=na_rm))
+}
+
+
+#' @name summarize with NA
+#' @export
+#' @importFrom dplyr first
+#' @aliases first_na
+first_na <- function(x,na_rm=FALSE) {
+
+	if(all(is.na(x))){
+		return(x[1])
+	} 
+
+	if(na_rm){
+		x <- na.omit(x)
+	}
+	
+	return(first(x))	
+
 }
