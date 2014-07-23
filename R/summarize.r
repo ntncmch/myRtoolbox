8496e1cad7de4aa6aedef90041bccc4b9606b6b8
Anton <anton.camacho@lshtm.ac.uk>
2014-07-23 20:15:44 +0100

    new min/max functions

--- R/summarize.r
+++ R/summarize.r
@@ -1,7 +1,7 @@
 #'Basic accounting for NA values
 #'
-#'All these functions performs the same as the basic version but differ when all elements are \code{NA}. In this case, \code{NA} is returned in the same format as \code{x}, which is not always the case when the basic function is used with \code{\link[dplyr]{summarize}}.
-#' @param x vector
+#'All these functions performs the same as the basic version (same name without \code{_na} at the end) but differ when all elements are \code{NA}. In this case, \code{NA} is returned in the same format as \code{x}, which is not always the case when the basic function is used with \code{\link[dplyr]{summarize}}.
+#' @param x any object accepted by the basic function.
 #' @param na_rm logical. If \code{TRUE}, \code{\link{na.omit}} is performed first. Default to \code{FALSE}
 #' @export
 #' @importFrom dplyr last
@@ -52,3 +52,41 @@ first_na <- function(x,na_rm=FALSE) {
 	return(first(x))	
 
 }
+
+#' @name summarize with NA
+#' @export
+#' @aliases max_na
+max_na <- function(x,na_rm=FALSE) {
+
+	if(all(is.na(x))){
+		return(x[1])
+	} 
+
+	if(na_rm){
+		x <- na.omit(x)
+	}
+	
+	return(max(x))	
+
+}
+
+
+#' @name summarize with NA
+#' @export
+#' @aliases min_na
+min_na <- function(x,na_rm=FALSE) {
+
+	if(all(is.na(x))){
+		return(x[1])
+	} 
+
+	if(na_rm){
+		x <- na.omit(x)
+	}
+	
+	return(min(x))	
+
+}
+
+
+
